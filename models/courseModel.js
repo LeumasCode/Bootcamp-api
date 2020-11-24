76bdf991c374533f6e8d16cec077f18ab62ee8a1
unknown <opeyemisamuel222@gmail.com>
2020-11-24 08:11:53 +0100

    create a static method on courses model to calculate the average cost of a bootcamp base on the tuition

--- models/courseModel.js
+++ models/courseModel.js
@@ -38,6 +38,46 @@ const courseSchema = new mongoose.Schema({
   },
 });
 
+// Static function to get average of course tuition fees
+courseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log('Calculating average Cost...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+
+  console.log(obj);
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(
+      bootcampId,
+      {
+        averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+      },
+      { new: true }
+    );
+  } catch (error) {
+    console.error(error);
+  } 
+};
+
+// Call getAverageCost after 'save'
+courseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before 'remove'
+courseSchema.pre('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 const Course = mongoose.model('Course', courseSchema);
 
 module.exports = Course;
