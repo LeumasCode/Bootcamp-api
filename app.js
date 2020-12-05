128e2a9854a70f0ecc1b119e8f4fbc238e7ecbd4
unknown <opeyemisamuel222@gmail.com>
2020-12-05 18:34:59 +0100

    set header security with helmet and prevent xss attacks

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 // route files
 const bootcampRoute = require('./routes/bootcampRoutes');
 const courseRoute = require('./routes/courseRoutes');
@@ -30,6 +32,12 @@ app.use(fileUpload());
 
 // Sanitize Data
 app.use(mongoSanitize());
+
+// Set security Headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss())
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
